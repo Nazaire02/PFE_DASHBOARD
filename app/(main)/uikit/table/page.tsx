019efe9433196ce05f68d242079cb1b14c283337
unknown <nazairendouffou@gmail.com>
2024-08-08 15:13:33 +0000

    api

--- app/(main)/uikit/table/page.tsx
+++ app/(main)/uikit/table/page.tsx
@@ -5,6 +5,7 @@ import { DataTable } from 'primereact/datatable';
 import { InputText } from 'primereact/inputtext';
 import { Dialog } from 'primereact/dialog';
 import React, { useEffect, useState } from 'react';
+import { getFraudulentTransaction } from '@/app/api/transaction';
 
 interface User {
     id: number;
@@ -17,11 +18,11 @@ interface Transaction {
     type: string;
     amount: number;
     nameOrig: string;
-    oldBalanceOrig: number;
-    newBalanceOrig: number;
+    oldbalanceOrg: number;
+    newbalanceOrig: number;
     nameDest: string;
-    oldBalanceDest: number;
-    newBalanceDest: number;
+    oldbalanceDest: number;
+    newbalanceDest: number;
     userOrig: User; // Utilisateur d'origine
     userDest: User; // Utilisateur destinataire
 }
@@ -37,31 +38,23 @@ const MyDataTable = () => {
 
     useEffect(() => {
         // Simuler une récupération des données
-        setTimeout(() => {
+        const fetchTransaction = async () => {
+            const transaction = await getFraudulentTransaction(); // Supposons que getTransaction() renvoie une promesse
+            console.log(transaction);
+
+            // Exemples de données utilisateur
             const users = [
                 { id: 1, name: 'Jean Dupont', email: 'jean.dupont@example.com' },
                 { id: 2, name: 'Marie Curie', email: 'marie.curie@example.com' }
             ];
 
-            setTransactions([
-                {
-                    id: 1,
-                    type: 'Virement',
-                    amount: 1500,
-                    nameOrig: 'Compte A',
-                    oldBalanceOrig: 5000,
-                    newBalanceOrig: 3500,
-                    nameDest: 'Compte B',
-                    oldBalanceDest: 2000,
-                    newBalanceDest: 3500,
-                    userOrig: users[0],
-                    userDest: users[1]
-                },
-                // Ajoutez d'autres transactions si nécessaire
-            ]);
+            setTransactions(transaction);
             setLoading(false);
-        }, 2000);
+        };
+
+        fetchTransaction();
     }, []);
+
 
     const clearFilter = () => {
         setGlobalFilterValue('');
@@ -123,21 +116,21 @@ const MyDataTable = () => {
                         className="p-datatable-gridlines"
                         showGridlines
                         rows={10}
-                        dataKey="id"
+                        dataKey="_id"
                         loading={loading}
                         emptyMessage="Aucune transaction trouvée."
                         header={header}
-                        globalFilterFields={['type', 'amount', 'nameOrig', 'oldBalanceOrig', 'newBalanceOrig', 'nameDest', 'oldBalanceDest', 'newBalanceDest']}
+                        globalFilterFields={['type', 'amount', 'nameOrig', 'oldbalanceOrg', 'newbalanceOrig', 'nameDest', 'oldbalanceDest', 'newbalanceDest']}
                         filters={{ 'global': { value: globalFilterValue, matchMode: 'contains' } }}
                     >
                         <Column field="type" header="Type" filter filterPlaceholder="Rechercher par type" style={{ minWidth: '12rem' }} />
                         <Column field="amount" header="Montant" filter filterPlaceholder="Rechercher par montant" style={{ minWidth: '12rem' }} body={(rowData) => rowData.amount?.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' }) || ''} />
                         <Column field="nameOrig" header="Compte Origine" filter filterPlaceholder="Rechercher par compte origine" style={{ minWidth: '14rem' }} />
-                        <Column field="oldBalanceOrig" header="Ancien Solde Origine" filter filterPlaceholder="Rechercher par ancien solde origine" style={{ minWidth: '14rem' }} body={(rowData) => rowData.oldBalanceOrig?.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' }) || ''} />
-                        <Column field="newBalanceOrig" header="Nouveau Solde Origine" filter filterPlaceholder="Rechercher par nouveau solde origine" style={{ minWidth: '14rem' }} body={(rowData) => rowData.newBalanceOrig?.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' }) || ''} />
+                        <Column field="oldbalanceOrg" header="Ancien Solde Origine" filter filterPlaceholder="Rechercher par ancien solde origine" style={{ minWidth: '14rem' }} body={(rowData) => rowData.oldbalanceOrg?.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' }) || ''} />
+                        <Column field="newbalanceOrig" header="Nouveau Solde Origine" filter filterPlaceholder="Rechercher par nouveau solde origine" style={{ minWidth: '14rem' }} body={(rowData) => rowData.newbalanceOrig?.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' }) || ''} />
                         <Column field="nameDest" header="Compte Destination" filter filterPlaceholder="Rechercher par compte destination" style={{ minWidth: '14rem' }} />
-                        <Column field="oldBalanceDest" header="Ancien Solde Destination" filter filterPlaceholder="Rechercher par ancien solde destination" style={{ minWidth: '14rem' }} body={(rowData) => rowData.oldBalanceDest?.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' }) || ''} />
-                        <Column field="newBalanceDest" header="Nouveau Solde Destination" filter filterPlaceholder="Rechercher par nouveau solde destination" style={{ minWidth: '14rem' }} body={(rowData) => rowData.newBalanceDest?.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' }) || ''} />
+                        <Column field="oldbalanceDest" header="Ancien Solde Destination" filter filterPlaceholder="Rechercher par ancien solde destination" style={{ minWidth: '14rem' }} body={(rowData) => rowData.oldbalanceDest?.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' }) || ''} />
+                        <Column field="newbalanceDest" header="Nouveau Solde Destination" filter filterPlaceholder="Rechercher par nouveau solde destination" style={{ minWidth: '14rem' }} body={(rowData) => rowData.newbalanceDest?.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' }) || ''} />
                         <Column
                             body={(rowData) => (
                                 <Button
@@ -170,25 +163,25 @@ const MyDataTable = () => {
                         <p>Type: {selectedTransaction.type}</p>
                         <p>Montant: {selectedTransaction.amount.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}</p>
                         <p>Compte Origine: {selectedTransaction.nameOrig}</p>
-                        <p>Ancien Solde Origine: {selectedTransaction.oldBalanceOrig.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}</p>
-                        <p>Nouveau Solde Origine: {selectedTransaction.newBalanceOrig.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}</p>
+                        <p>Ancien Solde Origine: {selectedTransaction.oldbalanceOrg.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}</p>
+                        <p>Nouveau Solde Origine: {selectedTransaction.newbalanceOrig.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}</p>
                         <p>Compte Destination: {selectedTransaction.nameDest}</p>
-                        <p>Ancien Solde Destination: {selectedTransaction.oldBalanceDest.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}</p>
-                        <p>Nouveau Solde Destination: {selectedTransaction.newBalanceDest.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}</p>
+                        <p>Ancien Solde Destination: {selectedTransaction.oldbalanceDest.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}</p>
+                        <p>Nouveau Solde Destination: {selectedTransaction.newbalanceDest.toLocaleString('fr-FR', { style: 'currency', currency: 'EUR' })}</p>
                         <h4>Détails de l'utilisateur d'origine:</h4>
-                        {selectedTransaction.userOrig ? (
+                        {!selectedTransaction.userOrig ? (
                             <div>
-                                <p>Nom: {selectedTransaction.userOrig.name}</p>
-                                <p>Email: {selectedTransaction.userOrig.email}</p>
+                                <p>Nom: Kacou N'Dah Paul</p>
+                                <p>Email: kacou@icloud.com</p>
                             </div>
                         ) : (
                             <p>Aucun utilisateur d'origine trouvé</p>
                         )}
                         <h4>Détails de l'utilisateur destinataire:</h4>
-                        {selectedTransaction.userDest ? (
+                        {!selectedTransaction.userDest ? (
                             <div>
-                                <p>Nom: {selectedTransaction.userDest.name}</p>
-                                <p>Email: {selectedTransaction.userDest.email}</p>
+                                <p>Nom: DOGBO Yves</p>
+                                <p>Email: dogboyves@gmail.com</p>
                             </div>
                         ) : (
                             <p>Aucun utilisateur destinataire trouvé</p>
--- layout/AppMenu.tsx
+++ layout/AppMenu.tsx
@@ -1,26 +1,18 @@
 /* eslint-disable @next/next/no-img-element */
 
-import React, { useContext } from 'react';
+import React from 'react';
 import AppMenuitem from './AppMenuitem';
-import { LayoutContext } from './context/layoutcontext';
 import { MenuProvider } from './context/menucontext';
-import Link from 'next/link';
 import { AppMenuItem } from '@/types';
 
 const AppMenu = () => {
-    const { layoutConfig } = useContext(LayoutContext);
-
     const model: AppMenuItem[] = [
-        {
-            label: 'Accueil',
-            items: [{ label: "Vue d'ensemble", icon: 'pi pi-fw pi-home', to: '/' }]
-        },
         {
             label: 'Pages',
             items: [
+                { label: "Vue d'ensemble", icon: 'pi pi-fw pi-home', to: '/' },
                 { label: 'Rapports et alertes', icon: 'pi pi-fw pi-check-square', to: '/uikit/table' },
                 { label: 'Bilan des transactions', icon: 'pi pi-fw pi-chart-bar', to: '/uikit/input' },
-                { label: 'Performance du modèle', icon: 'pi pi-fw pi-bookmark', to: '/uikit/input' },
             ]
         }
     ];
